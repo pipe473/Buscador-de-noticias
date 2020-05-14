233f69450cb32c0f5089c25beac8b7dffef72bac
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-14 17:31:21 +0200

    Creando componente header, pasando props de titulo y añadiendo estilos con materialize

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({titulo}) => {
+    return ( 
+        <nav className="nav-wrapper light-blue darken-3">
+            
+        </nav>
+     );
+}
+ 
+export default Header;

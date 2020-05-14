3707227cd79d9a9c7f098a71f974f869c708b84f
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-14 17:39:40 +0200

    Primeros pasos creando el componente de formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Formulario = () => {
+    return ( 
+        <div className="row">
+            <div className="col s12 m8 offset-m2">
+                <form>
+                    <h2>Encuentra Noticias por categoría</h2>
+                </form>
+            </div>
+        </div>
+     );
+}
+ 
+export default Formulario;

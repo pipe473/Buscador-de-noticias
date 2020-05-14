df51701f7925f64bd5f01a0203bcc7802717481d
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-14 17:42:35 +0200

    Añadiendo el componente formulario al archivo principal del proyecto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       />
 
       <div className="container white">
-        <h1>Formulario aquí</h1>
+        <Formulario />
       </div>
     </Fragment>
   );

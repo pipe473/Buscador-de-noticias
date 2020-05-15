c45ea3a4f06881d71119e32ed23369d07ab618fc
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-15 15:40:16 +0200

    Creando un custom hook para el select

---
+++ src/components/hooks/useSelect.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const useSelect = (stateInicial, opciones) => {
+   
+    const SelectNoticias = () => (
+        <select
+            className="browser-default"
+
+        >
+            <option value="">Seleccione</option>
+        </select>
+    );
+}
+ 
+export default useSelect;

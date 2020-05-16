90ce0305e0389be44af16e96613bffacc23f6c79
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-16 17:28:03 +0200

    Enseñando las imagenes y sus titulos desde la API

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const Noticia = () => {
-    return ( <p>noticia</p> );
+const Noticia = ({noticia}) => {
+
+// Extraer los datos desde la API
+const { urlToImage, url, title, description, source } = noticia;
+
+    return ( 
+        <div className="col s12 m6 l4">
+            <div className="card">
+                <img src={urlToImage} />
+
+                <div className="card-content">
+                    <h3>{title}</h3>
+                </div>
+            </div>
+        </div>
+     );
 }
  
 export default Noticia;

c694f437a8619770a4156f333683f5470bec8788
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-16 17:11:13 +0200

    Creando componente de ListadoNoticias y iterando desde noticias, para ir mostrando las noticias

---
+++ src/components/ListadoNoticias.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ListadoNoticias = ({noticias}) => {
+    return ( 
+        <div className="row">
+            {noticias.map(noticia => (
+                
+            ))}
+        </div>
+     );
+}
+ 
+export default ListadoNoticias;

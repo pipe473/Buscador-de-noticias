778f61e4187faf292f6f99a226d374cd912df483
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-16 17:38:03 +0200

    Añadiendo descripcion y un boton inferior con clases de materialize

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -9,11 +9,20 @@ const { urlToImage, url, title, description, source } = noticia;
         <div className="col s12 m6 l4">
             <div className="card">
                 <div className="card-image">
-                    <img src={urlToImage} />
+                    <img src={urlToImage} alt={title} />
                 </div>
 
                 <div className="card-content">
                     <h3>{title}</h3>
+                    <p>{description}</p>
+                </div>
+                <div className="card-action">
+                    <a
+                        href={url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="waves-effect waves-light btn"
+                    >Ver noticia completa</a>
                 </div>
             </div>
         </div>

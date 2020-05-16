2173e6fbfb77a12c6c887b41ce7ea4786fe579db
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-16 10:28:02 +0200

    Pasando las opciones por el custom hook

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -7,16 +7,16 @@ const Formulario = () => {
 
     const OPCIONES = [
         { value: 'general', label: 'General'},
-        { value: 'general', label: 'General'},
-        { value: 'general', label: 'General'},
-        { value: 'general', label: 'General'},
-        { value: 'general', label: 'General'},
-        { value: 'general', label: 'General'},
-        { value: 'general', label: 'General'},
+        { value: 'business', label: 'Negocios'},
+        { value: 'entertainment', label: 'Entretenimiento'},
+        { value: 'health', label: 'Salud'},
+        { value: 'science', label: 'Ciencia'},
+        { value: 'sports', label: 'Deportes'},
+        { value: 'technology', label: 'Tecnología'},
     ]
 
 // Utilizar custom hook
-const [ categoria, SelectNoticias ] = useSelect();
+const [ categoria, SelectNoticias ] = useSelect('general', OPCIONES);
 
     return ( 
         <div className={`${styles.buscador} row`}>

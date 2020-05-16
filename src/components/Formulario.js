8f7fab288d8fa7f0fb47000f37e4ab9b75cb4e62
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-16 17:51:41 +0200

    Documentando el Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Formulario.module.css';
 import useSelect from '../hooks/useSelect';
 
@@ -47,6 +48,11 @@ const buscarNoticias = e => {
             </div>
         </div>
      );
+}
+
+
+Formulario.propTypes = {
+    guardarCategoria: PropTypes.func.isRequired
 }
  
 export default Formulario;

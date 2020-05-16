606da326f8aaa67dd659a0ecf676e1e38a00a582
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-16 17:13:48 +0200

    Creando componente de Noticia para pasarlo por el componente de ListadoNoticia

--- src/components/ListadoNoticias.js
+++ src/components/ListadoNoticias.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Noticia from './Noticia';
 
 const ListadoNoticias = ({noticias}) => {
     return ( 
         <div className="row">
             {noticias.map(noticia => (
-                
+                <Noticia />
             ))}
         </div>
      );
